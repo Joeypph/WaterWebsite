51bfb6162e101141616939e37dda08072ddbdb2a
Joeypph <joseant_1098@hotmail.com>
2021-05-28 00:37:48 -0500

    almost final

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
-let button= document.getElementById("icono");
-let enlaces = document.getElementById("enlaces");
-let contador=0;
+//$(document).ready(function(){
 
-button.addEventListener("click",function(){
-    console.log("hi");
-    if(contador==0){
-        enlaces.className=("enlaces dos")
-        contador=1;
-    }else{
+    let button= document.getElementById("icono");
+    let enlaces = document.getElementById("enlaces");
+    var esconder = document.getElementById("esconder");
+    let contador=0;
+    
+    button.addEventListener("click",function(e){
+        e.preventDefault();
+        if(contador==0){
+            enlaces.className=("enlaces dos")
+            contador=1;
+        }else{
+            enlaces.classList.remove("dos")
+            enlaces.className=("enlaces uno")
+            contador=0;
+        }
+    })
+    esconder.addEventListener('click', function() {
+        //alert('Hello world');
         enlaces.classList.remove("dos")
         enlaces.className=("enlaces uno")
         contador=0;
+    });
 
-    }
-})
+
+//})
+
